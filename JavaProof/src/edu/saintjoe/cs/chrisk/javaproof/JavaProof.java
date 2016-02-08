dd14d81049590c48dceaf1751a09cf6bab5606b9
chrisk <ckv9508@saintjoe.edu>
2016-02-08 14:40:04 -0500

    I have conquered this task

---
+++ JavaProof/src/edu/saintjoe/cs/chrisk/javaproof/JavaProof.java
@@ -0,0 +1,10 @@
+package edu.saintjoe.cs.chrisk.javaproof;
+
+public class JavaProof {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I have conquered this task!!!");
+	}
+
+}
